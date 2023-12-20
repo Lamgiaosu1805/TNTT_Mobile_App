2fd1937c34317f99dd31319b65adf0f914ce7723
Lamgiaosu1805 <admin@vinasupport.com>
2023-12-20 18:08:16 +0700

    config build

--- src/utils.js
+++ src/utils.js
@@ -2,7 +2,7 @@
 const devApi = "http://192.168.0.8:3000/api/v1";
 const productApi = "https://tgp-hn-api.vercel.app/api/v1";
 class Utils {
-    apiUrl = devApi;
+    apiUrl = productApi;
 
     formatDate(date) {
         return new Date(date).toLocaleDateString('en-GB'); 
