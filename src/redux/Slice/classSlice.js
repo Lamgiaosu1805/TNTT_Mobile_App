f544d01d6416546da82df316fb0edfd67556f80e
Lamgiaosu1805 <admin@vinasupport.com>
2024-01-29 14:15:53 +0700

    fix bug logout

--- src/redux/Slice/classSlice.js
+++ src/redux/Slice/classSlice.js
@@ -1,8 +1,8 @@
 import { createSlice } from '@reduxjs/toolkit'
-
+const initialState = []
 const classes = createSlice({
     name: 'classes',
-    initialState: [],
+    initialState: initialState,
     reducers: {
         storeClass: (state, action) => {
             state = action.payload
